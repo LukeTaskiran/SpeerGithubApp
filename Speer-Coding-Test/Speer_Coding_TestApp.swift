52d3ae75bed6e79f53d34b8103b36af78fce34dc
Luke Taskiran <ltaskira@uwo.ca>
2025-04-04 21:07:14 -0400

    Initial commit

---
+++ Speer-Coding-Test/Speer_Coding_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speer_Coding_TestApp.swift
+//  Speer-Coding-Test
+//
+//  Created by Luke Taskiran on 2025-04-04.
+//
+
+import SwiftUI
+
+@main
+struct Speer_Coding_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
